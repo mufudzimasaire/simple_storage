632950643f899c7d131a71b26ef96ec522cdf846
Mufudzi Masaire <mufudzimasaire@gmail.com>
2021-09-25 19:06:24 +0100

    feature(main): adds ability to read provided args and inserting the data in the DB

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod storage;
+
+use std::{env::Args, iter::Skip};
+use storage::Database;
+
 fn main() {
-	println!("Hello world");
+	// Extract arguments
+	let mut args: Skip<Args> = std::env::args().skip(1);
+	let key = args.next().expect("Invalid key input");
+	let value = args.next().expect("Invalid value input");
+
+	// Store imput data
+	let _result = Database::insert(key, value);
 }
