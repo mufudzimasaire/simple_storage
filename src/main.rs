4a0575321db12fccde4c819250a71f6ef28ae246
Mufudzi Masaire <mufudzimasaire@users.noreply.github.com>
2021-09-26 06:47:12 +0100

    fix(database): fix indentation & general clean up

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,17 @@ use std::{env::Args, iter::Skip};
 use storage::Database;
 
 fn main() {
-	// Extract arguments
-	let mut args: Skip<Args> = std::env::args().skip(1);
-	let key = args.next().expect("Invalid key input");
-	let value = args.next().expect("Invalid value input");
+  // Extract arguments
+  let mut args: Skip<Args> = std::env::args().skip(1);
+  let key = args.next().expect("Invalid key input");
+  let value = args.next().expect("Invalid value input");
 
-	// Initialize DB
-	let mut db = Database::new().expect("Could not initialize database");
+  // Initialize DB
+  let mut db = Database::new().expect("Could not initialize database");
 
-	// Store input data
-	db.insert(key, value);
+  // Store input data
+  db.insert(key, value);
 
-	// Read all entries
-	db.read_all()
+  // Read all entries
+  db.read_all()
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,61 +1,64 @@
 use std::collections::HashMap;
+use std::fs::{read_to_string, write};
+use std::io::Error;
+use std::path::Path;
 
 type Key = String;
 type Value = String;
 
 pub struct Database {
-	data: HashMap<Key, Value>
+  data: HashMap<Key, Value>
 }
 
 impl Database {
-	/**
-	 * Instantiates the DB
-	 */
-	pub fn new() -> Result<Database, std::io::Error> {
-		let mut map = HashMap::new();
-		let db_name = "database.db";
+  /**
+   * Instantiates the DB
+   */
+  pub fn new() -> Result<Database, Error> {
+    let mut map = HashMap::new();
+    let db_name = "database.db";
 
-		if  std::path::Path::new(db_name).exists() {
-			// Read stored data
-			let stored_data = std::fs::read_to_string(db_name).unwrap();
+    if  Path::new(db_name).exists() {
+      // Read stored data
+      let stored_data = read_to_string(db_name).unwrap();
 
-			// Populate map from the retrieved data
-			for line in stored_data.lines() {
-				let (key, value) = line.split_once('\t').expect("Database is corrupted!");
-				map.insert(key.to_owned(), value.to_owned());
-			}
-		}
+      // Populate map from the retrieved data
+      for line in stored_data.lines() {
+        let (key, value) = line.split_once('\t').expect("Database is corrupted!");
+        map.insert(key.to_owned(), value.to_owned());
+      }
+    }
 
-		// Return database
-		Ok(Database { data: map })
-	}
+    // Return database
+    Ok(Database { data: map })
+  }
 
-	/**
-	 * Inserts a `key` and `value` pair into the DB
-	 */
-	pub fn insert(&mut self, key: Key, value: Value) {
-		// Insert new data in the DB
-		self.data.insert(key.to_lowercase(), value);
+  /**
+   * Inserts a `key` and `value` pair into the DB
+   */
+  pub fn insert(&mut self, key: Key, value: Value) {
+    // Insert new data in the DB
+    self.data.insert(key.to_lowercase(), value);
 
-		// Build data for storage
-		let mut new_data = String::new();
+    // Build data for storage
+    let mut new_data = String::new();
 
-		for (key, value) in &self.data {
-			new_data.push_str(&format!("{}\t{}\n", key, value));
-		}
+    for (key, value) in &self.data {
+      new_data.push_str(&format!("{}\t{}\n", key, value));
+    }
 
-		// Store data
-		let _result = std::fs::write("database.db", new_data);
-	}
+    // Store data
+    let _result = write("database.db", new_data);
+  }
 
-	/**
-	 * Returns all DB entries
-	 */
-	pub fn read_all (&self) {
-		println!("All entries");
+  /**
+   * Returns all DB entries
+   */
+  pub fn read_all (&self) {
+    println!("All entries");
 
-		for (key, value) in &self.data {
-			println!("{}: {}", key, value)
-		}
-	}
+    for (key, value) in &self.data {
+      println!("{}: {}", key, value)
+    }
+  }
 }
